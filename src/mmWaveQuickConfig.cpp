5c1cdfc05531e777113ed7b61982a5866fd800ec
marcojob <marco.job@bluewin.ch>
2021-05-13 11:29:27 +0200

    fix format of mmWaveQuickCOnfig

--- src/mmWaveQuickConfig.cpp
+++ src/mmWaveQuickConfig.cpp
@@ -15,40 +15,39 @@ int main(int argc, char **argv) {
         return 1;
     } else 
         ROS_INFO("mmWaveQuickConfig: Configuring mmWave device using config file: %s", argv[1]);
-    
+
     ros::ServiceClient client = nh.serviceClient<ti_mmwave_rospkg::mmWaveCLI>("/mmWaveCLI");
     std::ifstream myParams;
     ti_mmwave_rospkg::ParameterParser parser;
     //wait for service to become available
-    ros::service::waitForService("/mmWaveCLI", 10000); 
-    
+    ros::service::waitForService("/mmWaveCLI", 10000);
+
     //wait 0.5 secs to avoid multi-sensor conflicts
     ros::Duration(0.5).sleep();
 
     myParams.open(argv[1]);
-    
+
     if (myParams.is_open()) {
-        while( std::getline(myParams, srv.request.comm)) {
-        // Remove Windows carriage-return if present
-        srv.request.comm.erase(std::remove(srv.request.comm.begin(), srv.request.comm.end(), '\r'), srv.request.comm.end());
-        // Ignore comment lines (first non-space char is '%') or blank lines
-        if (!(std::regex_match(srv.request.comm, std::regex("^\\s*%.*")) || std::regex_match(srv.request.comm, std::regex("^\\s*")))) {
-            // ROS_INFO("mmWaveQuickConfig: Sending command: '%s'", srv.request.comm.c_str() );
-            if (client.call(srv)) {
-            if (std::regex_search(srv.response.resp, std::regex("Done"))) {
-                // ROS_INFO("mmWaveQuickConfig: Command successful (mmWave sensor responded with 'Done')");            
-                parser.ParamsParser(srv, nh);
-            } else {
-                ROS_ERROR("mmWaveQuickConfig: Command failed (mmWave sensor did not respond with 'Done')");
-                ROS_ERROR("mmWaveQuickConfig: Response: '%s'", srv.response.resp.c_str() );
-                return 1;
+        while(std::getline(myParams, srv.request.comm)) {
+            srv.request.comm.erase(std::remove(srv.request.comm.begin(), srv.request.comm.end(), '\r'), srv.request.comm.end());
+            // Ignore comment lines (first non-space char is '%') or blank lines
+            if (!(std::regex_match(srv.request.comm, std::regex("^\\s*%.*")) || std::regex_match(srv.request.comm, std::regex("^\\s*")))) {
+                ROS_INFO("mmWaveQuickConfig: Sending command: '%s'", srv.request.comm.c_str() );
+                if (client.call(srv)) {
+                    if (std::regex_search(srv.response.resp, std::regex("Done"))) {
+                        ROS_INFO("mmWaveQuickConfig: Command successful (mmWave sensor responded with 'Done')");
+                        parser.ParamsParser(srv, nh);
+                    } else {
+                        ROS_ERROR("mmWaveQuickConfig: Command failed (mmWave sensor did not respond with 'Done')");
+                        ROS_ERROR("mmWaveQuickConfig: Response: '%s'", srv.response.resp.c_str() );
+                        return 1;
+                    }
+                } else {
+                    ROS_ERROR("mmWaveQuickConfig: Failed to call service mmWaveCLI");
+                    ROS_ERROR("%s", srv.request.comm.c_str() );
+                    return 1;
+                }
             }
-            } else {
-            ROS_ERROR("mmWaveQuickConfig: Failed to call service mmWaveCLI");
-            ROS_ERROR("%s", srv.request.comm.c_str() );
-            return 1;
-            }
-        }
         }
         parser.CalParams(nh);
         myParams.close();
